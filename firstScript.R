1ef13de3fdd6664b2c1a58422c5562640f586ff6
Kadia Toure <kadiditoure@gmail.com>
2020-10-09 18:25:53 +0200

    ajout nouvelle fontion de split des auteurs

--- firstScript.R
+++ firstScript.R
@@ -1,29 +1,66 @@
 library(readxl) # Import des fichiers Excel
 install.packages("tidyverse")
-library(tidyverse)
+install.packages("tidyr")
+library(tidyr)
 
 #imports des données
-file.exists('~/R/projects/BigDataProject/data/excel/savedrecs.xlsx')
-wofs <- read_xlsx("~/R/projects/BigDataProject/data/excel/savedrecs.xlsx",sheet = "Feuil1")
-authorsNames <- wofs["Author Full Names"]
-#head(wofs)
-colnames(authorsNames)[1] <- "Authors"
-#supress comas
-#authorsNames <- authorsNames %>%   separate (Authors, c("Source", "Target"))
+file.exists('~/R/projects/BigDataProject/data/excel/csv-neurotrans-set.csv')
+pubmed <- read.csv(file = '~/R/projects/BigDataProject/data/excel/csv-neurotrans-set.csv')
 
-i <- 0
 
-while (i<nrow(authorsNames)) {
- print(authorsNames[i, ])
-  row <- authorsNames[i, ]
-  #split ';' create a table and multiple row by authors
+i <- 3
+row <- pubmed[i, ]
+authors<- row["Authors"]
+authors
+#res<-unlist(strsplit(toString(authors$Authors), split=","))
+#length(res)
+
+
+
+#create a data frame for output
+output <- data.frame("hi","bye")
+names(output)<-c("Source","Target")
+output
+
+
+
+i <- 1
+while (i<nrow(pubmed)) {
+  #get Authors splited 
+  row <- pubmed[i, ]
+  authors<- row["Authors"]
+  res<-unlist(strsplit(toString(authors$Authors), split=","))
+  
+  #adding relations 
+  j <- 1
+  if(length(res) == 2){
+    #rbind(output, list(res[j][1], res[j+1][1]))
+    #create a dataFrame
+    relationAuthor<-data.frame(res[j][1],res[j+1][1])
+    names(relationAuthor)<-c("Source","Target")
+    output <- rbind(output, relationAuthor)
+  }
+  else if(length(res) > 2){
+    while ((j+1)<length(res)){
+      k<-j+1
+      while(k<=length(res)){
+        relationAuthor<-data.frame(res[j][1],res[k][1])
+        names(relationAuthor)<-c("Source","Target")
+        output <- rbind(output, relationAuthor)
+        k<-k+1
+      }
+      j<- j + 1
+    }
+  }
+  
+  
   i <- i + 1
-} # fin tant que
+} 
 
 
 
-colnames(authorsNames)[1] <- "Source"
-colnames(authorsNames)[2] <- "Target"
+#colnames(authorsNames)[1] <- "Source"
+#colnames(authorsNames)[2] <- "Target"
 
 #export
-write.csv (authorsNames, file = "export/my_data.csv", row.names=FALSE)
+write.csv (output, file = "export/my_data.csv", row.names=FALSE)
